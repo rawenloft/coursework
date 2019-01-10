8d24768a62e08e71881bd1c6740c13dcf62758b3
Oleksiy Khrenov <oleksiykhrenov@MacBook-Pro-Oleksiy.local>
2019-01-10 17:41:25 +0200

    Two blocks looks little better. Add some files

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var scrolled;
+window.onscroll = function() {
+    scrolled = window.pageYOffset;
+    if(scrolled > 100){
+        $(".menu-wrapper").css({"background": "black","box-shadow": "0 0 12px 0 #2d2d2d"});
+    }
+    if(100 > scrolled){
+        $(".menu-wrapper").css({"background": "transparent","box-shadow": "none"})
+    }
+
+}
