61220f2208fafd13a3572f387aa6d527d92138a8
rawenloft <rawen.2000@gmail.com>
2019-01-11 15:26:50 +0200

    Done block 3, add anim and count script. Go to block 4.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var scrolled;
 window.onscroll = function() {
-    scrolled = window.pageYOffset;
+    scrolled = window.pageYOffset || document.documentElement.scrollTop;
     if(scrolled > 100){
         $(".menu-wrapper").css({"background": "black","box-shadow": "0 0 12px 0 #2d2d2d"});
     }
@@ -9,3 +9,14 @@ window.onscroll = function() {
     }
 
 }
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 8000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
