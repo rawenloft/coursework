48bc7ee383cf4a585c09c068902b92ce9bc7013b
rawenloft <rawen.2000@gmail.com>
2019-01-14 21:06:23 +0200

    Almost done block 9, go to b10

--- js/script.js
+++ js/script.js
@@ -9,14 +9,5 @@ window.onscroll = function() {
     }
 
 }
-$('.count').each(function () {
-    $(this).prop('Counter',0).animate({
-        Counter: $(this).text()
-    }, {
-        duration: 8000,
-        easing: 'swing',
-        step: function (now) {
-            $(this).text(Math.ceil(now));
-        }
-    });
-});
+
+
