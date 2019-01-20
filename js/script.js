698652d5e886a7f1c0c77f87d4bbcd4733c245b7
rawenloft <rawen.2000@gmail.com>
2019-01-20 16:44:37 +0200

    Minor fixes, add return button

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ window.onscroll = function() {
     scrolled = window.pageYOffset || document.documentElement.scrollTop;
     if(scrolled > 100){
         $(".menu-wrapper").css({"background": "black","box-shadow": "0 0 12px 0 #2d2d2d"});
+        $(".back").css({"opacity":"1"});
     }
     if(100 > scrolled){
-        $(".menu-wrapper").css({"background": "transparent","box-shadow": "none"})
+        $(".menu-wrapper").css({"background": "transparent","box-shadow": "none"});
+        $(".back").css({"opacity":"0"});
     }
 
-}
+};
