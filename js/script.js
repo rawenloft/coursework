a0c48fa1bbb22a43b5c915079409aeb44f25aa01
rawenloft <rawen.2000@gmail.com>
2019-01-20 17:55:13 +0200

    Minor files change and Parallax fixed))

--- js/script.js
+++ js/script.js
@@ -11,3 +11,67 @@ window.onscroll = function() {
     }
 
 };
+// Первая карусель
+		$(document).ready(function() {
+		 $("#myCarousel").owlCarousel({
+		 nav:false,
+		 loop:true,
+		 dots:false,
+		 dotData:false,
+		 pagination:false,
+		 autoplay:true,
+		 smartSpeed:1000,
+		 autoplayTimeout:4000,
+		 responsive:{ 
+                    0:{
+                        items:1
+                    },
+                    768:{
+                        items:2
+                    },
+                    1000:{
+                        items:3
+                    }
+                }
+		})
+	});
+	// Вторая карусель
+	$(document).ready(function() {
+		 $("#myCarouselTwo").owlCarousel({
+		 nav:false,
+		 loop:true,
+		 dots:true,
+		 dotData:true,
+		 pagination:true,
+		 autoplay:true,
+		 smartSpeed:1500,
+		 autoplayTimeout:6000,
+		 responsive:{
+                    0:{
+                        items:1
+                    },
+                    768:{
+                        items:1
+                    },
+                    1000:{
+                        items:1
+                    }
+                }
+		})
+	});
+	// Циферки
+	$('.count').each(function () {
+		"use strict"
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 10000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+   $(document).ready(function() {
+        $(window).stellar();
+    });
