ee37b35d12aecd745c4103590defe0d2ffd3a963
rawenloft <rawen.2000@gmail.com>
2019-02-03 13:10:43 +0200

    Fixed img filter, add photo, fix some ipsums. Going to next step

--- js/script.js
+++ js/script.js
@@ -101,10 +101,16 @@ window.
     		e.preventDefault();
     		$(".sort-images li span").siblings().removeClass('blink');
     		$(this).siblings().addClass('blink');
-                var $grid = $('.grid').isotope({});
-                var filterValue = $(this).attr('data-filter');
-                $grid.isotope({ filter: filterValue });
             
-
+            var filterValue = $(this).attr('data-filter');
+            var elem = $(".img-box");
+                if(filterValue == "all"){
+                  $(elem).show("1000").css({'transform': 'translate(50)'});
+                }
+                else{
+                  $(elem).not("."+filterValue).hide("1000").fadeOut();
+                  $(elem).filter("."+filterValue).show("1000").fadeIn();
+                }
+                
     	});
     });
